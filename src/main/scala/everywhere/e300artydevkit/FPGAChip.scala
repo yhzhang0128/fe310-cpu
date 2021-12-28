8a472f277166fd9e3a8a8b614494ad8c0e745475
Yunhao Zhang <yz2327@cornell.edu>
2021-12-28 17:35:29 -0500

    Increase the clock from 32MHz to 65MHz

--- src/main/scala/everywhere/e300artydevkit/FPGAChip.scala
+++ src/main/scala/everywhere/e300artydevkit/FPGAChip.scala
@@ -37,7 +37,7 @@ class E300ArtyDevKitFPGAChip(implicit override val p: Parameters) extends ArtySh
   // DUT
   //-----------------------------------------------------------------------
 
-  withClockAndReset(clock_32MHz, ck_rst) {
+  withClockAndReset(clock_65MHz, ck_rst) {
     val dut = Module(new E300ArtyDevKitPlatform)
 
     //---------------------------------------------------------------------
